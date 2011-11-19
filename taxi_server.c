5e7d2df030eaa4029a17a93890b5b8c85585cf9b
A.R Karthick <a.r.karthick@gmail.com>
2011-11-18 22:51:59 -0800

    send on 0 taxi matches

--- taxi_server.c
+++ taxi_server.c
@@ -40,7 +40,7 @@ static int send_taxi_list(struct taxi *taxi, struct taxi *taxis, int num_taxis, 
     unsigned char *taxis_marker = s;
     s += sizeof(unsigned int);
     int max_taxis =  __MAX_PACKET_LEN/(sizeof(struct taxi)  + _TAXI_OVERHEAD);
-    if(num_taxis >= max_taxis) 
+    if(num_taxis > max_taxis) 
     {
         num_taxis = max_taxis;
         *(unsigned int*)taxis_marker = htonl(num_taxis);
